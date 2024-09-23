96ed1605822d4c09911b02c1ea178d79a01fb9ba
Serkan USLU <serkanuslu.info@gmail.com>
2024-09-24 00:01:41 +0300

    feat: bundle analyzer

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+import withBundleAnalyzer from '@next/bundle-analyzer';
+
+const bundleAnalyzer = withBundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+const nextConfig = {
+  reactStrictMode: true,
+};
+
+export default bundleAnalyzer(nextConfig);
